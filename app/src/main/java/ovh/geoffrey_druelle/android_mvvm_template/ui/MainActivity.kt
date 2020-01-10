de01112d7875f5ef0cec9ab7e24747c2f6c12cf5
Geoffrey Druelle <gdruelle@timcod.fr>
2020-01-10 11:40:55 +0100

    Initate the repository with a customized build.gradle

---
+++ app/src/main/java/ovh/geoffrey_druelle/android_mvvm_template/ui/MainActivity.kt
@@ -0,0 +1,13 @@
+package ovh.geoffrey_druelle.android_mvvm_template.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import ovh.geoffrey_druelle.android_mvvm_template.R
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
